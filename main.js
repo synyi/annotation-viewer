fa8cffbd9d8fe828677727f8af1515b55506189f
yjh <yjhatfdu@gmail.com>
2017-01-17 11:07:24 +0800

    merge

--- main.js
+++ main.js
@@ -1,11 +1,54 @@
 /**
  * Created by yjh on 2016/10/8.
  */
-System.register(['./node_modules/synyi-annotation-tool/src/Annotator'], function(exports_1, context_1) {
+System.register(["./node_modules/synyi-annotation-tool/src/Annotator"], function (exports_1, context_1) {
     "use strict";
+    var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+        return new (P || (P = Promise))(function (resolve, reject) {
+            function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+            function rejected(value) { try { step(generator.throw(value)); } catch (e) { reject(e); } }
+            function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+            step((generator = generator.apply(thisArg, _arguments)).next());
+        });
+    };
+    var __generator = (this && this.__generator) || function (thisArg, body) {
+        var _ = { label: 0, sent: function() { if (sent[0] === 1) throw sent[1]; return sent[1]; }, trys: [], stack: [] }, sent, f;
+        function step(op) {
+            if (f) throw new TypeError("Generator is already executing.");
+            while (1) {
+                if (_.done) switch (op[0]) {
+                    case 0: return { value: void 0, done: true };
+                    case 1: case 6: throw op[1];
+                    case 2: return { value: op[1], done: true };
+                }
+                try {
+                    switch (f = 1, op[0]) {
+                        case 0: case 1: sent = op; break;
+                        case 4: return _.label++, { value: op[1], done: false };
+                        case 7: op = _.stack.pop(), _.trys.pop(); continue;
+                        default:
+                            var r = _.trys.length > 0 && _.trys[_.trys.length - 1];
+                            if (!r && (op[0] === 6 || op[0] === 2)) { _.done = 1; continue; }
+                            if (op[0] === 3 && (!r || (op[1] > r[0] && op[1] < r[3]))) { _.label = op[1]; break; }
+                            if (op[0] === 6 && _.label < r[1]) { _.label = r[1], sent = op; break; }
+                            if (r && _.label < r[2]) { _.label = r[2], _.stack.push(op); break; }
+                            if (r[2]) { _.stack.pop(); }
+                            _.trys.pop();
+                            continue;
+                    }
+                    op = body.call(thisArg, _);
+                }
+                catch (e) { op = [6, e]; }
+                finally { f = 0, sent = void 0; }
+            }
+        }
+        return {
+            next: function (v) { return step([0, v]); },
+            "throw": function (v) { return step([1, v]); },
+            "return": function (v) { return step([2, v]); }
+        };
+    };
     var __moduleName = context_1 && context_1.id;
-    var Annotator_1;
-    var categories;
     function loadContentOfFile(file) {
         var fr = new FileReader();
         return new Promise(function (resolve) {
@@ -15,60 +58,123 @@ System.register(['./node_modules/synyi-annotation-tool/src/Annotator'], function
             fr.readAsText(file, 'utf-8');
         });
     }
-    function run() {
-        document.querySelector('#con').innerHTML = '';
-        var contentInput = document.querySelector('input#content');
-        var annotationInput = document.querySelector('input#annotation');
-        Promise.all([loadContentOfFile(contentInput.files[0]), loadContentOfFile(annotationInput.files[0])])
-            .then(function (results) {
-            var ann = new Annotator_1.Annotator(document.querySelector('#con'));
-            var annotations = JSON.parse(results[1]);
-            var relations = [];
-            var counter = 0;
-            var labels = annotations['concepts'].map(function (concept) {
-                concept['relations'] = concept['relations'] || [];
-                concept['relations'].forEach(function (relation) {
-                    if (relation['attribute_id'] != undefined) {
-                        relations.push({
-                            id: counter,
-                            dst: concept['meta']['id'],
-                            src: relation['attribute_id'],
-                            text: relation['relation_type']
-                        });
-                        counter += 1;
-                    }
-                });
-                return {
-                    'category': Annotator_1.Categories[concept['meta']['category']],
-                    'pos': [concept['meta']['start_index'], concept['meta']['end_index'] - 1],
-                    'id': concept['meta']['id']
-                };
-            }).filter(function (x) { return !(x['category'] == undefined); });
-            ann.import(results[0], categories, labels, relations);
+    function jsonp(url, callbackName) {
+        if (!callbackName) {
+            callbackName = '__callback__' + Math.round(Math.random() * 1000000);
+        }
+        return new Promise(function (resolve, reject) {
+            window[callbackName] = function (content) {
+                resolve(content);
+                document.body.removeChild(scp);
+                window[callbackName] = undefined;
+            };
+            var scp = document.createElement('script');
+            scp.src = url + callbackName;
+            scp.onerror = reject;
+            document.body.appendChild(scp);
         });
     }
+    function run() {
+        return __awaiter(this, void 0, void 0, function () {
+            var contentInput, result, nlpResult, rel_labels, relationDict, category_labels, text, concepts, labels, relations, rid;
+            return __generator(this, function (_a) {
+                switch (_a.label) {
+                    case 0:
+                        contentInput = document.querySelector('#content');
+                        return [4 /*yield*/, loadContentOfFile(contentInput.files[0])];
+                    case 1:
+                        result = _a.sent();
+                        nlpResult = JSON.parse(result);
+                        return [4 /*yield*/, annotationService.getRelation()];
+                    case 2:
+                        rel_labels = _a.sent();
+                        relationDict = {};
+                        rel_labels.forEach(function (x) { return relationDict[x.id] = x.text; });
+                        return [4 /*yield*/, annotationService.getCategory()];
+                    case 3:
+                        category_labels = _a.sent();
+                        text = nlpResult.rawText;
+                        concepts = nlpResult.NLPResult.concepts;
+                        labels = concepts.map(function (x) {
+                            return {
+                                category: x.category,
+                                pos: [x.pos[0], x.pos[1] - 1],
+                                id: x.id
+                            };
+                        });
+                        relations = [];
+                        rid = 0;
+                        concepts.forEach(function (x) {
+                            x.relations.forEach(function (r) {
+                                relations.push({
+                                    src: r.src_id,
+                                    text: relationDict[r.type],
+                                    id: rid++,
+                                    dst: x.id
+                                });
+                            });
+                        });
+                        ann.import(text, category_labels, labels, relations);
+                        return [2 /*return*/];
+                }
+            });
+        });
+    }
+    var Annotator_1, AnnotationService, annotationService, ann;
     return {
-        setters:[
+        setters: [
             function (Annotator_1_1) {
                 Annotator_1 = Annotator_1_1;
-            }],
-        execute: function() {
-            categories = [
-                { id: 1, fill: 'rgb(174, 214, 241)', boader: 'rgb(93, 173, 226)', highlight: 'rgba(174, 214, 241,0.4)', text: "症状、表现", },
-                { id: 2, fill: 'rgb(169, 204, 227)', boader: 'rgb(84, 153, 199)', highlight: 'rgba(169, 204, 227,0.4)', text: "疾病", },
-                { id: 3, fill: 'rgb(210, 180, 222)', boader: 'rgb(165, 105, 189)', highlight: 'rgba(210, 180, 222,0.4)', text: "检查、评分", },
-                { id: 4, fill: 'rgb(215, 189, 226)', boader: 'rgb(175, 122, 197)', highlight: 'rgba(215, 189, 226,0.4)', text: "治疗", },
-                { id: 5, fill: 'rgb(245, 183, 177)', boader: 'rgb(236, 112, 99)', highlight: 'rgba(245, 183, 177,0.4)', text: "指标", },
-                { id: 6, fill: 'rgb(230, 176, 170)', boader: 'rgb(205, 97, 85)', highlight: 'rgba(230, 176, 170,0.4)', text: "药物", },
-                { id: 7, fill: 'rgb(237, 187, 153)', boader: 'rgb(245, 176, 65)', highlight: 'rgba(237, 187, 153,0.4)', text: "部位、方位", },
-                { id: 8, fill: 'rgb(245, 203, 167)', boader: 'rgb(244, 208, 63)', highlight: 'rgba(245, 203, 167,0.4)', text: "频率", },
-                { id: 9, fill: 'rgb(250, 215, 160)', boader: 'rgb(252, 220, 160)', highlight: 'rgba(250, 215, 160,0.4)', text: "值", },
-                { id: 10, fill: 'rgb(171, 235, 198)', boader: 'rgb(181, 222, 190)', highlight: 'rgba(171, 235, 198,0.4)', text: "症状变化", },
-                { id: 11, fill: 'rgb(169, 223, 191)', boader: 'rgb(175, 220, 190)', highlight: 'rgba(169, 223, 191,0.4)', text: "其他修饰词" },
-                { id: 12, fill: 'rgb(249, 231, 159)', boader: 'rgb(82, 190, 128)', highlight: 'rgba(249, 231, 159,0.4)', text: "时间", },
-            ];
+            }
+        ],
+        execute: function () {/**
+             * Created by yjh on 2016/10/8.
+             */
+            AnnotationService = (function () {
+                function AnnotationService() {
+                }
+                AnnotationService.prototype.getRelation = function () {
+                    return __awaiter(this, void 0, void 0, function () {
+                        var _a;
+                        return __generator(this, function (_b) {
+                            switch (_b.label) {
+                                case 0:
+                                    if (!!this.relCache)
+                                        return [3 /*break*/, 2];
+                                    _a = this;
+                                    return [4 /*yield*/, jsonp('https://yi-ai.cn/api/category/relation/export?callback=')];
+                                case 1:
+                                    _a.relCache = _b.sent();
+                                    _b.label = 2;
+                                case 2: return [2 /*return*/, this.relCache];
+                            }
+                        });
+                    });
+                };
+                AnnotationService.prototype.getCategory = function () {
+                    return __awaiter(this, void 0, void 0, function () {
+                        var _a;
+                        return __generator(this, function (_b) {
+                            switch (_b.label) {
+                                case 0:
+                                    if (!!this.catCache)
+                                        return [3 /*break*/, 2];
+                                    _a = this;
+                                    return [4 /*yield*/, jsonp('https://yi-ai.cn/api/category/label/export?callback=')];
+                                case 1:
+                                    _a.catCache = _b.sent();
+                                    _b.label = 2;
+                                case 2: return [2 /*return*/, this.catCache];
+                            }
+                        });
+                    });
+                };
+                return AnnotationService;
+            }());
+            annotationService = new AnnotationService();
+            ann = new Annotator_1.Annotator(document.querySelector('#con'));
             document.querySelector('#run').addEventListener('click', function () { return run(); });
         }
-    }
+    };
 });
 //# sourceMappingURL=main.js.map
